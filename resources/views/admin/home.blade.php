ad1c894bf10d11a69e2514946366a5c263f2f293
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-11-21 15:41:33 +0100

    created controller and model for posts

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1 class="text-center text-uppercase py-5 text-primary">Posts</h1>
     <div class="d-flex justify-content-center">
+        <a class="btn btn-success mx-2" href="">Posts</a>
         <a class="btn btn-primary" href="">Create Post</a>
     </div>
 @endsection
