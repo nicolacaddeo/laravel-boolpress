09467f1bb5e72b74b795507aff6027373f974e4a
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-11-21 15:27:30 +0100

    added admin dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app');
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+@section('content')
+    <h1 class="text-center text-uppercase py-5 text-primary">Posts</h1>
+    <div class="d-flex justify-content-center">
+        <a class="btn btn-primary" href="">Create Post</a>
+    </div>
+@endsection
