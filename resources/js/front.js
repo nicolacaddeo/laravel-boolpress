96a71ccc798116d22bd0a5c47e2f52bb0c0d0b61
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-11-24 23:11:12 +0100

    added and configured vue-router

--- resources/js/front.js
+++ resources/js/front.js
@@ -5,8 +5,10 @@ window.Vue = require('vue');
 window.axios = require('axios');
 
 import App from "./views/App";
+import router from "./router";
 
 const app = new Vue({
     el: '#root',
     render: h => h(App),
+    router
 });
